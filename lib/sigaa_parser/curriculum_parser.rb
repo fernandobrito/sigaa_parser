6ff34828d1d098b67a8c22ae2b0761004936ec51
Fernando Brito <email@fernandobrito.com>
2016-01-14 14:26:51 +0100

    Move logic from Parser to Curriculum parser

--- lib/sigaa_parser/curriculum_parser.rb
+++ lib/sigaa_parser/curriculum_parser.rb
@@ -2,14 +2,44 @@ require 'active_support/core_ext/array'
 
 module SigaaParser
   class CurriculumParser
+    # Name of the program
+    def initialize(name = nil)
+    end
+
+    def retrieve(agent)
+      headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
+
+      url = 'https://sigaa.ufpb.br/sigaa/portais/discente/discente.jsf'
+      payload = 'menu:form_menu_discente=menu:form_menu_discente&id=149639&jscook_action=menu_form_menu_discente_j_id_jsp_1030614539_62_menu:A]#{ curriculo.popularBuscaGeral }&javax.faces.ViewState=j_id2'
+
+      page = agent.post(url, payload, headers)
+      page.open_in_browser
+
+      url = 'https://sigaa.ufpb.br/sigaa/geral/estrutura_curricular/busca_geral.jsf'
+      payload = 'busca=busca&busca:checkCurso=on&busca:curso=1626669&busca:matriz=0&busca:codigo=&busca:j_id_jsp_648669378_450=Buscar&javax.faces.ViewState=j_id3'
+
+      page = agent.post(url, payload, headers)
+      page.open_in_browser
+
+      url = 'https://sigaa.ufpb.br/sigaa/graduacao/curriculo/lista.jsf'
+      payload = 'resultado=resultado&javax.faces.ViewState=j_id4&resultado%3Arelatorio=resultado%3Arelatorio&id=940'
+
+      page = agent.post(url, payload, headers)
+      page.open_in_browser
+
+      page
+    end
+
     def parse(page)
       courses = []
 
-      code = page.search("//th[contains(., 'Código')]/following-sibling::td[1]").text.strip
-      name = page.search("//th[contains(., 'Matriz Curricular')]/following-sibling::td[1]").text.strip
+      code = page.search("//th[contains(., 'Código')]/following-sibling::td[1]").text.remove_tabulation
+      name = page.search("//th[contains(., 'Matriz Curricular')]/following-sibling::td[1]").text.remove_tabulation
 
       # This is the third table
+      # STRANGE: cached page works with first line. Live page with second!
       table = page.search("#relatorio > table > tbody > tr > td > table")[2]
+      table ||= page.search("#relatorio > table > tr > td > table")
 
       # - 1 for 0th semester
       # - 1 because everything is inside one big td
--- lib/sigaa_parser/parser.rb
+++ lib/sigaa_parser/parser.rb
@@ -2,60 +2,47 @@ module SigaaParser
   class AuthenticationFailed < Exception ; end
 
   class Parser
+    attr_accessor :agent
+
     def initialize
       @agent = Mechanize.new
     end
 
     def authenticate(login, password)
       # Login
-      page = @agent.get('https://sigaa.ufpb.br/sigaa/verTelaLogin.do')
-      form = page.forms.first
+      fill_and_submit_login_form(login, password)
 
+      # Choose enrollment (optional?)
+      page = @agent.get('https://sigaa.ufpb.br/sigaa/escolhaVinculo.do?dispatch=escolher&vinculo=1')
+
+      # Parse student data
+      return parse_student_data(page)
+
+      get_program_structure
+    end
+
+    def fill_and_submit_login_form(login, password)
+      # Request page
+      page = @agent.get('https://sigaa.ufpb.br/sigaa/verTelaLogin.do')
+
+      # Fill form
+      form = page.forms.first
       form.field_with(name: 'user.login').value = login
       form.field_with(name: 'user.senha').value = password
-
       page = form.submit
 
       # Check if login worked
       if page.search('//*[@id="conteudo"]/center[2]').text.include?('inválidos')
         raise SigaaParser::AuthenticationFailed.new
       end
+    end
 
-      # Choose enrollment (optional?)
-      page = @agent.get('https://sigaa.ufpb.br/sigaa/escolhaVinculo.do?dispatch=escolher&vinculo=1')
-
-      # Save some data
+    def parse_student_data(page)
       id = page.search("//td[contains(., 'Matrícula')]/following-sibling::td[1]").text.remove_tabulation
       name = page.search('.nome').text.remove_tabulation
       program = page.search("//td[contains(., 'Curso:')]/following-sibling::td[1]").text.remove_tabulation
 
-      return SigaaParser::Student.new(id, name, program)
-
-      get_program_structure
-    end
-
-    def get_program_structure
-      headers = { 'Content-Type' => 'application/x-www-form-urlencoded' }
-
-      url = 'https://sigaa.ufpb.br/sigaa/portais/discente/discente.jsf'
-      #payload = 'menu%3Aform_menu_discente=menu%3Aform_menu_discente&id=149639&jscook_action=menu_form_menu_discente_j_id_jsp_1030614539_62_menu%3AA%5D%23%7B+curriculo.popularBuscaGeral+%7D&javax.faces.ViewState=j_id2'
-      payload = 'menu:form_menu_discente=menu:form_menu_discente&id=149639&jscook_action=menu_form_menu_discente_j_id_jsp_1030614539_62_menu:A]#{ curriculo.popularBuscaGeral }&javax.faces.ViewState=j_id2'
-
-      page = @agent.post(url, payload, headers)
-      page.open_in_browser
-
-      url = 'https://sigaa.ufpb.br/sigaa/geral/estrutura_curricular/busca_geral.jsf'
-      # payload = 'busca=busca&busca%3AcheckCurso=on&busca%3Acurso=1626669&busca%3Amatriz=0&busca%3Acodigo=162006&busca%3Aj_id_jsp_648669378_450=Buscar&javax.faces.ViewState=j_id3'
-      payload = 'busca=busca&busca:checkCurso=on&busca:curso=1626669&busca:matriz=0&busca:codigo=162006&busca:j_id_jsp_648669378_450=Buscar&javax.faces.ViewState=j_id3'
-
-      page = @agent.post(url, payload, headers)
-      page.open_in_browser
-
-      url = 'https://sigaa.ufpb.br/sigaa/graduacao/curriculo/lista.jsf'
-      payload = 'resultado=resultado&javax.faces.ViewState=j_id4&resultado%3Arelatorio=resultado%3Arelatorio&id=940'
-
-      page = @agent.post(url, payload, headers)
-      page.open_in_browser
+      SigaaParser::Student.new(id, name, program)
     end
   end
 end
