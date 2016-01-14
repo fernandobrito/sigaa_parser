c3a799f120bdc00bef5ff9c192f7cec9ce66279f
Fernando Brito <email@fernandobrito.com>
2016-01-14 11:04:14 +0100

    Spec for Parser

---
+++ spec/parser_spec.rb
@@ -0,0 +1,35 @@
+require 'yaml'
+
+CONFIG_FILE = File.join(File.dirname(__FILE__), '..', 'configuration.yaml')
+
+describe SigaaParser::Parser do
+  let(:student) {
+    SigaaParser::Student.new('11111309', 'FERNANDO SANTOS DE MATTOS BRITO',
+                             'CIÊNCIAS DA COMPUTAÇÃO (BACHARELADO)/CI - João Pessoa - MT')
+  }
+
+  describe '#authenticate' do
+    context 'when authenticating' do
+      let(:subject) do
+        config = YAML.load_file(CONFIG_FILE)
+
+        @login = config['credentials']['login']
+        @password = config['credentials']['password']
+
+        SigaaParser::Parser.new
+      end
+
+      context 'with correct data' do
+        it 'should find student information' do
+          expect(subject.authenticate(@login, @password)).to be == student
+        end
+      end
+
+      context 'with incorrect data' do
+        it 'should raise an error' do
+          expect { subject.authenticate('a', 'b') }.to raise_error(SigaaParser::AuthenticationFailed)
+        end
+      end
+    end
+  end
+end
