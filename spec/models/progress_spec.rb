be810648c6d68c43335d1c222b53c50e57e7e03a
Fernando Brito <email@fernandobrito.com>
2016-05-08 15:00:39 -0300

    Fix Progress specs

--- spec/models/progress_spec.rb
+++ spec/models/progress_spec.rb
@@ -7,14 +7,14 @@ describe SigaaParser::CourseResults::Progress do
   context 'when has done all credits' do
     it 'should calculate the percentage values' do
       subject.flexible.set(100, 100)
-      expect(subject.compulsory.percentage_completed).to eq(100)
+      expect(subject.flexible.percentage_completed).to eq(100)
     end
   end
 
   context 'when has done no credits' do
     it 'should calculate the percentage values' do
       subject.optional.set(0, 100)
-      expect(subject.compulsory.percentage_completed).to eq(0)
+      expect(subject.optional.percentage_completed).to eq(0)
     end
   end
 end
