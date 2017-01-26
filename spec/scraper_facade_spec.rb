4cc3545da29546b0a3002fc9c1619c9fbbedcd17
Fernando Brito <email@fernandobrito.com>
2017-01-26 11:06:43 +0100

    Fix name of variable on specs

--- spec/scraper_facade_spec.rb
+++ spec/scraper_facade_spec.rb
@@ -1,15 +1,15 @@
 describe SigaaParser::ScraperFacade, headless: true do
   describe '#parse_curricula' do
-    COURSE_NAME = 'CIÊNCIA DA COMPUTAÇÃO - João Pessoa - Presencial - MT - BACHARELADO'
+    PROGRAM_NAME = 'CIÊNCIA DA COMPUTAÇÃO - João Pessoa - Presencial - MT - BACHARELADO'
     subject { SigaaParser::ScraperFacade.new.parse_curricula(['1626669']) }
 
-    context 'given code for one course' do
+    context 'given code for one program' do
       it 'should return only one result' do
         expect(subject.size).to be(1)
       end
 
       it 'should have correct name' do
-        expect(subject.first.name).to eq(COURSE_NAME)
+        expect(subject.first.name).to eq(PROGRAM_NAME)
       end
 
       it 'should have all courses' do
